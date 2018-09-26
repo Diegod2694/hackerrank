6d2fc5e9d3c16aed22b03b4747e45792c831e09c
Diego Parra <diegod2694@gmail.com>
2018-09-26 14:31:37 -0400

    let and const challenge

---
+++ variables.js
@@ -0,0 +1,10 @@
+
+const calculate = (r) => {
+  const PI = Math.PI
+  const area = PI * r
+  const perimeter = 2 * PI * r
+  console.log(area)
+  console.log(perimeter)
+} 
+
+console.log(calculate(2))
