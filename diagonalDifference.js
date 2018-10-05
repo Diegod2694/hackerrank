24259db1c1bd2a1f16a3121d8d5d33325ce0ed52
Diego Parra <diegod2694@gmail.com>
2018-10-05 09:35:16 -0400

    WIP: finding number in the middle of the matrix

--- diagonalDifference.js
+++ diagonalDifference.js
@@ -1,22 +1,33 @@
 function diagonalDifference(arr) {
   const numberOfIterations = arr[0] * arr[0]
   const numberOfColumns = arr[0]
-  console.log(numberOfIterations)
+  let numberOfRows = arr[0]
   const newArr = arr.slice(1)
-  console.log('newArr', newArr)
   let matrix = []
   let row = []
-  accRow = 0
-  for (let i = 0; i < array.length; i++) {
+  let accRow = 0
+  for (let i = 0; i < numberOfColumns; i++) {
     row[i] = []
-    for (let j = accRow; j < array.length; j++) {
-      row[i].push(arr[j])
+    for (let j = accRow; j < numberOfRows; j++) {
+      row[i].push(newArr[(j)])
     }
     accRow = accRow + numberOfColumns
+    numberOfRows = numberOfRows + numberOfColumns
     matrix.push(row[i])
-    console.log('row[i]', row[i])
   }
-
+  console.log(`matrix`, matrix)
+  let first = 0
+  for (let i = 0; i < numberOfColumns; i++) {
+    for (let j = 0; j < numberOfColumns; j++) {
+      if (i === j) {
+        first = first + matrix[i][j]
+        console.log(first)
+      }
+      if ( i === 0 )[
+        
+      ]
+    }
+  }
 }
 
 const array = [ 3, 11, 2, 4, 4, 5, 6, 10, 8, -12 ]
